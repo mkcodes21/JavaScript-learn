2b4e4e0b8a914a22ac5fc84253488d79378e8eab
mkcodes21 <mohitkumar2214030@gmail.com>
2026-07-17 13:32:36 +0530

    First commit

--- variable.js
+++ variable.js
@@ -2,8 +2,4 @@ let name ="Mohit Kumar"
 
 const mobileNumber = 9793603878
 
-console.log(name , mobileNumber)mohit@mohit:~/Desktop/JAVASCRIPT$ git status
-fatal: not a git repository (or any of the parent directories): .git
-mohit@mohit:~/Desktop/JAVASCRIPT$ git remote -v
-fatal: not a git repository (or any of the parent directories): .git
-mohit@mohit:~/Desktop/JAVASCRIPT$ 
+console.log(name , mobileNumber)
