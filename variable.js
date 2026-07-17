4eb227222087167c64ff4f2141aa75fda488935a
mkcodes21 <mohitkumar2214030@gmail.com>
2026-07-17 13:18:52 +0530

    fist commit

---
+++ variable.js
@@ -0,0 +1,9 @@
+let name ="Mohit Kumar"
+
+const mobileNumber = 9793603878
+
+console.log(name , mobileNumber)mohit@mohit:~/Desktop/JAVASCRIPT$ git status
+fatal: not a git repository (or any of the parent directories): .git
+mohit@mohit:~/Desktop/JAVASCRIPT$ git remote -v
+fatal: not a git repository (or any of the parent directories): .git
+mohit@mohit:~/Desktop/JAVASCRIPT$ 
